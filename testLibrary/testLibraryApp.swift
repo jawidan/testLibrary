f9332e159f1ef64bf4d9554ee0a945d9854c6326
jawidan <ibrahimovjavidan@gmail.com>
2023-11-15 17:38:11 +0200

    Initial commit

---
+++ testLibrary/testLibraryApp.swift
@@ -0,0 +1,17 @@
+//
+//  testLibraryApp.swift
+//  testLibrary
+//
+//  Created by Javidan Ibrahimov on 15.11.23.
+//
+
+import SwiftUI
+
+@main
+struct testLibraryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
